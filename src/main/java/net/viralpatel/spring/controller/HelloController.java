7847ccb3ce498a902070b2158bf1081352616cac
junksbond007 <junksbond007@gmail.com>
2018-06-02 13:39:14 -0400

    Update HelloController.java

--- src/main/java/net/viralpatel/spring/controller/HelloController.java
+++ src/main/java/net/viralpatel/spring/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 	@GetMapping("/hello")
 	public String hello(Model model) {
 
-		model.addAttribute("name", "Md Zeyaul Haque");
+		model.addAttribute("name", "Md Zeyaul Haque finally");
 
 		return "welcome";
 	}
